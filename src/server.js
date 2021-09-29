e95f906cd2fa5ea511030cfbcba2517fd209a050
SiyanaKDimitrova <sdimitrova348@gmail.com>
2021-09-29 16:38:27 +0300

    update script

--- src/server.js
+++ src/server.js
@@ -12,7 +12,12 @@ app.set('view engine', 'hbs');
 
 app.get('/', function (req, res) {
     res.render('home', { layout: false });
+   
 });
+
+app.get('/cats/addBreed', (req, res) => {
+    res.render('addBreed', {layout: false})
+})
 
 app.listen(3000, console.log.bind(console, 'server runing on http://localhost:3000'));
 
